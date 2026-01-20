75c7bc0c0565d9161e619e8784c4a076ade91626
WilmaHSS <wilma.marbell@healthsharedservices.ca>
2026-01-20 14:11:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 
@@ -18,8 +17,8 @@ st.write("""Choose the fruits you want in your smoothie!""")
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be:', name_on_order)
 
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session= cnx.session()
 session.add_packages("dash-table","itables")     #add packages
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
