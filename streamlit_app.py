53da63640a25e4f8e02d90a07ff026beac0d3219
WilmaHSS <wilma.marbell@healthsharedservices.ca>
2026-01-21 10:52:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,6 @@ st.write('The name on your smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session= cnx.session()
 
-
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data = my_dataframe, use_container_width = True)
 #st.stop
@@ -29,7 +28,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #Convert the showpark data frame to a panda data frame so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+#st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
