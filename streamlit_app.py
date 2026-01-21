993b014fa38c1b7d81b777e3901d9e12a98da2e6
WilmaHSS <wilma.marbell@healthsharedservices.ca>
2026-01-20 17:46:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from snowflake.snowpark.functions import col
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = true)
 
 
 # Write directly to the app
