9db69a46ba7b9db984d059011d34d69efdb3c954
WilmaHSS <wilma.marbell@healthsharedservices.ca>
2026-01-21 10:23:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,11 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 st.dataframe(data = my_dataframe, use_container_width = True)
 #st.stop
 
+#Convert the showpark data frame to a panda data frame so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     my_dataframe,
