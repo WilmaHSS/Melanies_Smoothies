14cd9eb79d8b2940be3fead3e78dd2adaa022c9a
WilmaHSS <wilma.marbell@healthsharedservices.ca>
2026-01-21 09:04:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw:")
@@ -32,7 +29,6 @@ ingredients_list = st.multiselect(
     max_selections = 5
 )
 
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
 #for Phyton use 4 spaces instead of tab for indenting
 if ingredients_list:
@@ -40,8 +36,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
-   # st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
